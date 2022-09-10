8f0559900ebfc29e02336eec3b17e49a9e98e645
nebuchad-nezzar <vatsal.sharma1997@gmail.com>
2022-09-10 21:37:54 +0530

    package update

--- marketplace/tailwind.config.js
+++ marketplace/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   mode: "jit",
   purge: [
+    ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
